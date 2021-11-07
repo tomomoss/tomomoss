abd1d0f7c9c10f448a92e90999218d6774047063
tomomoss <ishida.tmys@gmail.com>
2021-11-07 16:11:54 +0900

    タイトルアニメーションの完成後の状態を作りました。

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+import {
+  playTitleAnimation
+} from "./title-animation/title-animation";
+
 /**
  * ヘッダーを表すHTML要素を取得します。
  * @returns {Element} ヘッダーを表すHTML要素です。
@@ -50,22 +54,24 @@ const implementInPageScrolling = (): void => {
 };
 
 /**
+ * 当ポートフォリオを構成する主要なコンテンツのmin-heightプロパティに適当な値を設定します。
+ * 具体的にはビューポートの高さからヘッダーの高さを引いた値が設定値となります。
+ */
+const implementProcessingThatResizeMinHeightOfContents = (): void => {
+  new ResizeObserver((entries: ResizeObserverEntry[], observer: ResizeObserver): void => {
+    for (const e of Array.from(document.querySelectorAll<HTMLElement>(".main__content"))) {
+      e.style.minHeight = `${document.documentElement.clientHeight - _header.scrollHeight}px`;
+    }
+  }).observe(document.documentElement);
+};
+
+/**
  * 当ポートフォリオを構成する主要なコンテンツ名のmargin-topプロパティに適当な値を設定します。
  * 具体的にはビューポートの高さと、コンテンツ名を視覚的にちょうどよい位置に置くための幅を設定します。
  */
 const setMarginTopOfContentTitles = (): void => {
   for (const e of Array.from(document.querySelectorAll<HTMLElement>(".main__content-title"))) {
     e.style.marginTop = `${_header.scrollHeight * 2}px`;
-  }
-};
-
-/**
- * 当ポートフォリオを構成する主要なコンテンツのmin-heightプロパティに適当な値を設定します。
- * 具体的にはビューポートの高さからヘッダーの高さを引いた値が設定値となります。
- */
-const setMinHeightOfContents = (): void => {
-  for (const e of Array.from(document.querySelectorAll<HTMLElement>(".main__content"))) {
-    e.style.minHeight = `${innerHeight - _header.scrollHeight}px`;
   }
 };
 
@@ -138,6 +144,9 @@ const _header = getHeader();
 // 初期化の順番は順不同で問題ありません（とりあえずアルファベット順に並べています）。
 implementCurrentContentStyleSetting();
 implementInPageScrolling();
+implementProcessingThatResizeMinHeightOfContents();
 setMarginTopOfContentTitles();
-setMinHeightOfContents();
 setPaddingTopOfProfilRow();
+
+// タイトルアニメーションを再生します。
+requestAnimationFrame(playTitleAnimation);
